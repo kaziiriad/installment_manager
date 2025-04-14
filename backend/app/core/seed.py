6694af56e42a06129e5472788dbe57455a66c451
Sultan Mahmud <kaziiriad@gmail.com>
2025-04-14 13:23:25 +0600

    Modified create admin configuration.

--- backend/app/core/seed.py
+++ backend/app/core/seed.py
@@ -1,17 +1,24 @@
-from .database import SessionLocal
+from pydantic import EmailStr
+from sqlalchemy import select
+from .database import AsyncSessionLocal, SessionLocal
 from models.db_models import User, Role
 from .security import get_password_hash
 from .config import settings
 
-def create_admin():
-    db = SessionLocal()
-    admin = db.query(User).filter(User.email == "admin@example.com").first()
-    if not admin:
-        admin = User(
-            email="admin@example.com",
-            hashed_password=get_password_hash("admin_password"),
-            role=Role.ADMIN,
-            is_verified=True  # Skip OTP for admin
-        )
-        db.add(admin)
-        db.commit()
+async def create_admin(admin_email: EmailStr):
+    async with AsyncSessionLocal() as db:
+        result = await db.execute(select(User).filter(User.email == admin_email))
+        admin = result.scalars().first()
+        
+        if not admin:
+            admin = User(
+                email=admin_email,
+                hashed_password=get_password_hash("admin_password"),
+                role=Role.ADMIN,
+                is_verified=True
+            )
+            db.add(admin)
+            await db.commit()
+        elif admin.role != Role.ADMIN:
+            admin.role = Role.ADMIN
+            await db.commit()
